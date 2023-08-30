6af94a8c35e2ee98527ed093515263f097862d13
MOSAN <mochammadichsan02@gmail.com>
2023-08-30 12:17:37 +0700

    Update CSS

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -61,7 +61,7 @@
                 justify-content: center;
                 align-items: center;
                 margin-top: 20px;
-                gap: 20px;
+                gap: 10px;
                 /* margin-bottom: 150px */
             }
 
