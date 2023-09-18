e7f5fa6c18ebafd66b0217e24b9bc46b09cb15ef
MOSAN <mochammadichsan02@gmail.com>
2023-09-18 13:25:12 +0700

    Qris Enable Button

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -162,10 +162,10 @@
                     <p class="card-text" style="font-size: 9pt">With supporting text below as a natural lead-in to
                         additional content.</p>
                 </div>
-                <a href="{{ url('kredit') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
-                        style="font-size: 1rem; color: white; margin-left: 0.5rem;"></i></a>
-                {{-- <a href="{{ route('maintenance') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
+                {{-- <a href="{{ url('kredit') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
                         style="font-size: 1rem; color: white; margin-left: 0.5rem;"></i></a> --}}
+                <a href="{{ route('maintenance') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
+                        style="font-size: 1rem; color: white; margin-left: 0.5rem;"></i></a>
             </div>
             <div class="card text-center">
                 <i class="bi bi-qr-code-scan icon-head" style=" color: rgb(0, 0, 0)"></i>
@@ -174,10 +174,10 @@
                     <p class="card-text lh-1" style="font-size: 9pt">With supporting text below as a natural lead-in to
                         additional content.</p>
                 </div>
-                {{-- <a href="{{ url('createqris') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
-                        style="font-size: 1rem; color: white; margin-left: 0.5rem;"></i></a> --}}
-                <a href="{{ route('maintenance') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
+                <a href="{{ url('createqris') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
                         style="font-size: 1rem; color: white; margin-left: 0.5rem;"></i></a>
+                {{-- <a href="{{ route('maintenance') }}" class="btn btn-primary">Pilih <i class="bi bi-arrow-right-short"
+                        style="font-size: 1rem; color: white; margin-left: 0.5rem;"></i></a> --}}
             </div>
         </div>
     </div>
